24931ca75290d1d947cf2c7ca78703a6df7b163a
Kodeine <cubinngo1102@gmail.com>
2021-05-09 12:36:12 +0700

    setup project ok

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import photoReducer from 'features/Photo/photoSlice';
+// import userReducer from "./userSlice";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const rootReducer = {
+  photos: photoReducer,
+  // user: userReducer,
+}
+
+const store = configureStore({
+  reducer: rootReducer,
 });
+
+export default store;
